80507c1dc3f6caba5b57a991e16f923df5256139
Gabriel Aguilar <gabonfc1650@gmail.com>
2025-01-16 03:14:56 -0500

    Clase 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
+import { WorldComponent } from './world/world.component';
 
 export const routes: Routes = [
-    /*{ 
-        path: 'hello', loadChildren: () => import('./hello/hello.module').then(m => m.HelloModule) 
-    }*/
-    {
+    { 
         path: '',
         component: HelloComponent
+    },
+    {
+        path: '',
+        component: WorldComponent
     }
 ];
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { WorldComponent } from '../world/world.component';
 
 @Component({
   selector: 'app-hello',
-  imports: [FormsModule],
+  imports: [FormsModule, WorldComponent],
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.css'
 })
 export class HelloComponent {
 
   nombre: string = '';
-   
+  mensajeDelHijo: string = '';
+  listaNombres = [
+    {nombre: 'Juan', apellido: 'Perez'}
+    ,{nombre: 'Maria', apellido: 'Gomez'}
+    ,{nombre: 'Pedro', apellido: 'Gonzalez'}
+    ,{nombre: 'Ana', apellido: 'Lopez'}
+    ,{nombre: 'Luis', apellido: 'Martinez'}
+    ,{nombre: 'Laura', apellido: 'Rodriguez'}
+    ,{nombre: 'Carlos', apellido: 'Sanchez'}
+    ,{nombre: 'Sofia', apellido: 'Ramirez'}
+    ,{nombre: 'Diego', apellido: 'Hernandez'}
+    ,{nombre: 'Valentina', apellido: 'Torres'}
+  ]
+  onEmit(mensaje: string) {
+    this.mensajeDelHijo = mensaje;
+    window.alert('Mensaje recibido desde el hijo: ' + mensaje);
+  }
 }
